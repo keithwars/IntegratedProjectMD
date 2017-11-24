68e18fe83b3ab8a5ca38e7a7adf3b6758c6100cc
JeremyKeusters <jeremy@keusters.be>
2017-11-24 12:18:44 +0100

    Initial project setup

---
+++ BuzzyMail/BuzzyMail/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BuzzyMail
+//
+//  Created by Jérémy Keusters on 24/11/17.
+//  Copyright © 2017 Jérémy Keusters. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BuzzyMail/BuzzyMail/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BuzzyMail
+//
+//  Created by Jérémy Keusters on 24/11/17.
+//  Copyright © 2017 Jérémy Keusters. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
