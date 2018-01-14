85c4c0b98b0298416e2d663992ccd87b6a9810c2
Compulsor <schelfhoutlennart@gmail.com>
2018-01-14 19:42:39 +0100

    Storyboard addition to add contact

---
+++ BuzzyMail/BuzzyMail/ContactsAddContactViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ContactsAddContactViewController.swift
+//  BuzzyMail
+//
+//  Created by Lennart Schelfhout on 14/01/2018.
+//  Copyright © 2018 Jérémy Keusters. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ContactsAddContactViewController: UITableViewController, UITextFieldDelegate {
+}
